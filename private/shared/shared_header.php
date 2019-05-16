37fad8126d7e63aa2855d8cfdc61204bee240644
noamdom <ndomovich@gmail.com>
2019-05-16 16:01:18 +0300

    make fine host page 1

--- private/shared/shared_header.php
+++ private/shared/shared_header.php
@@ -2,9 +2,9 @@
     if (!isset($page_title)) {
         $page_title = '--';
     }
-    if ($page_title != 'login') {
-        require_login();
-    }
+//    if ($page_title != 'login') {
+//        require_login();
+//    }
 ?>
 
 
---
+++ public_html/find-host.php
@@ -0,0 +1,21 @@
+<?php
+require_once('../private/init.php');
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//
+}
+?>
+<?php $page_title = 'find / host act'; ?>
+<?php include SHARED_PATH . '/shared_header.php' ?>
+
+   <main>
+       <h1>find host act</h1>
+   </main>
+
+
+
+<?php
+include SHARED_PATH . '/shared_footer.php';
