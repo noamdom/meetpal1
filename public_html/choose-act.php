f3f8e14f0eeb069bcac78583eeae30a0cd75c966
noamdom <ndomovich@gmail.com>
2019-05-16 16:07:39 +0300

    make choose act page1

---
+++ public_html/choose-act.php
@@ -0,0 +1,21 @@
+<?php
+require_once('../private/init.php');
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//
+}
+?>
+<?php $page_title = 'choose act'; ?>
+<?php include SHARED_PATH . '/shared_header.php' ?>
+
+    <main>
+        <h1>choose act</h1>
+    </main>
+
+
+
+<?php
+include SHARED_PATH . '/shared_footer.php';
