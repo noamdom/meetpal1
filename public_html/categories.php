70c1d0ef3e74ef83e2102b64e3bf6f595340f66c
noamdom <ndomovich@gmail.com>
2019-05-16 16:05:49 +0300

    make categories page1

---
+++ public_html/categories.php
@@ -0,0 +1,21 @@
+<?php
+require_once('../private/init.php');
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//
+}
+?>
+<?php $page_title = 'categories'; ?>
+<?php include SHARED_PATH . '/shared_header.php' ?>
+
+    <main>
+        <h1>categories</h1>
+    </main>
+
+
+
+<?php
+include SHARED_PATH . '/shared_footer.php';
